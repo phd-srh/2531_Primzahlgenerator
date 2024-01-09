f123fb0c4e3871e93f0b15739b38b14cf5523cde
Dr. Dillinger <peter.dillinger@srh.de>
2024-01-09 11:25:24 +0100

    Programm ist soweit fertig, eine kleine Hausaufgabe gibt es aber noch...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Primzahlgenerator primzahlgenerator = new Primzahlgenerator(100);
+        System.out.println( primzahlgenerator.erzeugePrimzahl() );
+        System.out.println( primzahlgenerator.erzeugePrimzahl() );
+        System.out.println( primzahlgenerator.erzeugePrimzahl() );
 
+        /*
+         * Man könnte jetzt noch das Programm erweitern:
+         * - Startwert vom Anwender erfragen
+         * - Anazhl der zu erzeugenden Primzahlen abfragen
+         */
     }
 }
--- src/Primzahlgenerator.java
+++ src/Primzahlgenerator.java
@@ -1,2 +1,21 @@
 public class Primzahlgenerator {
+    public int startwert;
+
+    public Primzahlgenerator(int startwert) {
+        this.startwert = startwert;
+    }
+
+    public int erzeugePrimzahl() {
+        do {
+            startwert++;
+        } while ( ! istPrimzahl(startwert) );
+        return startwert;
+    }
+
+    public boolean istPrimzahl(int zahl) {
+        for (int teiler = 2; teiler < zahl; teiler++) {
+            if (zahl % teiler == 0) return false;
+        }
+        return true;
+    }
 }
